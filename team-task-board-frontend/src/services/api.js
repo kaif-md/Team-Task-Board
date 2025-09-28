93fbd6769a3b01f6873b083d7f3d76354b82fc9a
kaif-md <217r1a05g6@cmrtc.ac.in>
2025-09-28 15:58:43 +0530

    again going back to baseUrl
    if i am changing the baseUrl my frontend  is showing error in deployment

--- team-task-board-frontend/src/services/api.js
+++ team-task-board-frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL
+  baseURL: "https://team-task-board-backend-yilz.onrender.com"
 });
 
 // Attach token automatically if available
